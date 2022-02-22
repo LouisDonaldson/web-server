4713a12a50f40ad514678e2b47cbf4c8486d8bc1
Louis Donaldson <71708797+SittingBanana01@users.noreply.github.com>
2022-02-22 10:53:32 +0000

    Update APICall.js

--- site/APICall.js
+++ site/APICall.js
@@ -1,17 +1,19 @@
 const submit_button = document.querySelector('#submit_btn_div');
 const definition_text = document.querySelector('#results_text');
+var added = false;
+
 
 // submit button clicked
 submit_button.addEventListener('click', function(){
   var input_text = document.querySelector('#input').value;
   if(input_text != "")
   {
+    added = false;
     GetResponse(input_text)
   }
 })
 
 var responseObj = null;
-var added = false;
 
 // api handler
 function GetResponse(input_text){
@@ -44,6 +46,8 @@ function AddDefinitionToUI(responseObj){
     // <div id="result_text_div">
     //      <h3 id="results_text" style="text-align: center; margin: 10px;">No results</h3>
     // </div>
+    document.querySelector('#main_div').innerHTML = "<div id=\"title_div\"><h2 style=\"text-align: center; margin: 0px 0px 20px 0px;\">Enter a word or phrase to learn it's Urban Dictionary Definition</h2></div><div id=\"input_submit_div\"><input id=\"input\" type=\"text\" placeholder=\"Enter a word or phrase\"><div id=\"submit_btn_div\"><h6 style=\"margin: 0px;\">submit</h6></div></div>";
+
     var individualDefinitionResponse = responseObj.list[i];
 
     var newResultsTextDiv =  document.createElement('div');
